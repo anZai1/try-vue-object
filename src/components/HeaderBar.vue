c7825654932a2bca1660fa3dad84f1970144d238
hujingan <812926755@qq.com>
2022-10-23 22:04:18 +0800

    第二次提交

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar",
+  data() {
+    return {
+      options: [
+        {
+          value: "001",
+          label: "修改",
+        },
+        {
+          value: "002",
+          label: "新增",
+        },
+        {
+          value: "003",
+          label: "删除",
+        },
+      ],
+      value: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.el-header {
+  background: skyblue;
+  width: 100%;
+  line-height: 60px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HeaderBar from '../components/HeaderBar'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HeaderBar',
+    component: HeaderBar
   },
   {
     path: '/about',
